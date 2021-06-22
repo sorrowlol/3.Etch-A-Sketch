c1da96194b99894a988f11091f20fbfd137c6694
sorrowlol <sorrowlol@outlook.com>
2021-06-23 00:21:21 +0200

    updated hover

--- js.js
+++ js.js
@@ -1,9 +1,19 @@
 let container = document.querySelector('#container');
 
 //create 16x16 grid
+
+
 for (let i = 0; i < 64; i++){
-    let b = document.createElement('div');
-    b.classList.add(i);
-    b.setAttribute('id','grid-divs')
-    container.appendChild(b);
+    let grid = document.createElement('div');
+    grid.classList.add('box');                     //adding a class to each box
+    grid.setAttribute('id', i);                    //adding same ID to all boxes
+    container.appendChild(grid);                   //appending to container div   
 }
+
+const boxes = document.querySelectorAll('.box');
+    
+boxes.forEach((div) => {
+    div.addEventListener('mouseover', (e) => {
+        e.target.style.backgroundColor = 'green';
+    });
+})

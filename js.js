b6395698cf34d025e8b33a0ee6e7c1ebffce38d4
sorrowlol <sorrowlol@outlook.com>
2021-06-24 23:02:58 +0200

    made new grid

--- js.js
+++ js.js
@@ -1,12 +1,11 @@
 let container = document.querySelector('#container');
 
-let rows = 16;
-let columns = 16;
-
-let size = rows*columns;
+let rows = 8;
+let columns = 8;
 
 function makeGrid(rows, columns){
     
+    let size = rows*columns;
     //pass variable to CSS
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-columns', columns);
@@ -15,8 +14,8 @@ function makeGrid(rows, columns){
         let c = document.createElement('div');
         c.classList.add('box');
         c.setAttribute('id', i);
-        c.style.height = 800/columns+"px"
-        c.style.width = 800/rows+"px";
+        c.style.height = 650/columns+"px"
+        c.style.width = 650/rows+"px";
         container.appendChild(c);
     }
 }
@@ -25,17 +24,33 @@ makeGrid(rows, columns);
 
 let boxes = document.querySelectorAll('.box');
 
+//draw
 boxes.forEach((div) => {
     div.addEventListener('mouseover', (e) => {
         e.target.style.backgroundColor = 'green';
     });
 });
 
+//reset drawing
 let reset = document.querySelector('#reset');
 
 reset.addEventListener('click', () => {
+    let ask = confirm("This will earse your drawing. Are you sure?")
+
+    if (ask){
     boxes.forEach((div) =>{
         div.style.backgroundColor = 'white';
     })
+}
+})
+
+//new grid
+let newGrid = document.querySelector('#new-grid');
+newGrid.addEventListener('click', () =>{
+    let ask = prompt('How many squares do you want?');
+    rows = Math.round(Math.sqrt(ask));
+    columns = Math.round(Math.sqrt(ask));
+    container.innerHTML = '';
+    makeGrid(rows, columns);
 })
 

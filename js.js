7c44c84ed625af4319ee7a28729e92e1d04308cb
sorrowlol <sorrowlol@outlook.com>
2021-07-18 20:27:05 +0200

    transparent mouseover

--- js.js
+++ js.js
@@ -24,7 +24,7 @@ function makeGrid(rows, columns){
     //draw
     boxes.forEach((x) => {
     x.addEventListener('mouseover', (e) => {
-        e.target.style.backgroundColor = `rgba(${Math.floor(Math.random()*512)}, ${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)}, 1)`;
+        e.target.style.backgroundColor = `rgba(${Math.floor(Math.random()*512)}, ${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)}, 0.7)`;
         });
     });
     
